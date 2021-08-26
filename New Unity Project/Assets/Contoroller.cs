ad594da2444379a34c41b09a647d97f399265855
Ryobb6 <78227356+Ryobb6@users.noreply.github.com>
2021-08-27 01:21:09 +0900

    Unity Project first commit
    first commit

---
+++ New Unity Project/Assets/Contoroller.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Contoroller : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Console.WriteLine("a");
+        Console.WriteLine("b");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
