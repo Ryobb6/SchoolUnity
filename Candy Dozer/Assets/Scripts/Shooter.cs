07cbd06a4e7858cacdb679c49be398013fbb3aa0
Ryobb6 <rryyoobb@gmail.com>
2021-09-22 15:22:08 +0900

    15220922
    1522

--- Candy Dozer/Assets/Scripts/Shooter.cs
+++ Candy Dozer/Assets/Scripts/Shooter.cs
@@ -7,17 +7,22 @@ public class Shooter : MonoBehaviour
     const int MaxShotPower = 5;
     const int RecoverySeconds = 3;
 
-    int shotPower = MaxShotPower;
+    private int shotPower = MaxShotPower;
+    private AudioSource shotSound; 
 
 
     public GameObject[] candyPrefabs;
-    public Transform candyParentTransform;
+    public Transform candyParentTransform; // InspectorからCandies (Candyの親をセット)
     public CandyManager candyManager;
     public float shotForce;
     public float shotTorque;
     public float baseWidth;
 
-   
+    private void Start()
+    {
+        shotSound = GetComponent<AudioSource>(); 
+    }
+
     // Update is called once per frame
     void Update()
     {
@@ -39,26 +44,38 @@ public class Shooter : MonoBehaviour
     /// <returns></returns>
     Vector3 GetInstantiatePosition()
     {
+        // The bottom-left of the screen or window is at (0, 0).
+        // The top-right of the screen or window is at (Screen.width, Screen.height).
         float x = baseWidth * (Input.mousePosition.x / Screen.width) - (baseWidth / 2);
-        return transform.position + new Vector3(x, 0, 0);
+        return transform.position + new Vector3(x, 0, 0); // Shoterオブジェクト(空のオブジェクト)からのxの位置を加算
     }
 
     public void Shot()
     {
         // キャンディを生成できない条件ならShotしない
-        if (candyManager.GetCandyAmount() <= 0) return;
-        if (shotPower <= 0) return; // ショットパワーのチェック
+        if (candyManager.GetCandyAmount() <= 0)
+        {
+            return;
+        }
+        if (shotPower <= 0)
+        {
+            return; // ショットパワーのチェック
+        }
 
         // PrefabからCandy Objectを生成　Gameオブジェクトはnewできない
-        GameObject candy = (GameObject)Instantiate(
-            SampleCandy(),
-            GetInstantiatePosition(),
-            // transform.position,
+        // 戻り値はGameObject型
+        // Genericsに戻り値のコンポーネント型を指定すると、
+        // そのGameObjectがhas-aしているコンポーネントを取得する
+
+        GameObject candy = Instantiate(
+            SampleCandy(),　// 生み出すGameObject
+            GetInstantiatePosition(), // 生み出すposition
             Quaternion.identity // Quaternionは回転を表現する構造体(.identityプロパティをGetすると、回転無し)
             );
 
 
-        // 生成したCandyオブジェクトの親をcandyParentTransformに設定
+        // 生成したCandyオブジェクトの親をcandyの親のTransformに設定
+        // 親はCandies
         candy.transform.parent = candyParentTransform;
         
         // Candy オブジェクトのRigidbodyを取得し力と回転を加える
@@ -70,6 +87,10 @@ public class Shooter : MonoBehaviour
 
         // Shotされた場合、Candyのストックを消費
         candyManager.ConsumeCandy();
+        ConsumePower();
+
+        // サウンドを再生
+        shotSound.Play();
 
     }
 
@@ -87,7 +108,7 @@ public class Shooter : MonoBehaviour
 
     }
 
-    private void CustomPower()
+    private void ConsumePower()
     {
         // ShotPowerを消費すると同時に回復のカウントをスタート
         shotPower--;
