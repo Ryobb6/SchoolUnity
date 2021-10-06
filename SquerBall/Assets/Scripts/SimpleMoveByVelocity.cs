b59b6d0f292d7228e85e3998446d364284e9c5fc
Ryobb6 <rryyoobb@gmail.com>
2021-10-06 12:50:32 +0900

    Addforceがきかない
    なぜ？

--- SquerBall/Assets/Scripts/SimpleMoveByVelocity.cs
+++ SquerBall/Assets/Scripts/SimpleMoveByVelocity.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class SimpleMoveByVelocity : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField]private float velocityX;
+    private Vector3 vector;
+    private Vector3 force;
+    private Rigidbody rg;
     void Start()
     {
-        
+        this.vector = new Vector3(this.velocityX,0, 0);
+        this.rg = GetComponent<Rigidbody>();
+        this.force = this.vector;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+         //  * Time.fixedDeltaTime*10
+        this.rg.AddForce(this.force, ForceMode.VelocityChange);
     }
 }
