acdfb16e005f064c856e7d1fcdbb7ae697ed63ee
Ryobb6 <rryyoobb@gmail.com>
2021-10-06 14:44:32 +0900

    TextMesh
    TextMesh

--- SquerBall/Assets/Scripts/CapselAreaDetector.cs
+++ SquerBall/Assets/Scripts/CapselAreaDetector.cs
@@ -2,12 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// エリアに入った物体に対して、動作をつけるスクリプトです
+/// </summary>
 public class CapselAreaDetector : MonoBehaviour
 {
+    [SerializeField] private bool isHolding; //UI変更用
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        isHolding = false;
+    }
+
+    public bool IsHolding
+    {
+        get
+        {
+            return this.isHolding;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        isHolding = true;
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        Rigidbody rg = other.gameObject.GetComponent<Rigidbody>();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        isHolding = false;
     }
 
     // Update is called once per frame
