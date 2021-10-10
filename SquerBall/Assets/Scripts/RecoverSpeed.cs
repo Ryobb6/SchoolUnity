5a4ea41ee5bf2fd85adff047ef3c471c44fc330c
Ryobb6 <78227356+Ryobb6@users.noreply.github.com>
2021-10-11 01:25:47 +0900

    123
    123

---
+++ SquerBall/Assets/Scripts/RecoverSpeed.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RecoverSpeed : MonoBehaviour
+{
+    public GameObject targetObject;
+    public float applySpeed;
+    // Start is called before the first frame update
+    private void OnTriggerExit(Collider other)
+    {
+        targetObject.GetComponent<SpaceShipController>().Speed = this.applySpeed;
+    }
+}
