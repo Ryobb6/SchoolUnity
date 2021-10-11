54dc7ee0267edd5381818d23f543dda211e0cb7f
Ryobb6 <rryyoobb@gmail.com>
2021-10-11 15:21:27 +0900

    15211011
    15211011 Practice

---
+++ SquerBall/Assets/Scripts/NewSpaceShipController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewSpaceShipController : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    private Transform tr;
+
+    public float Speed { get => speed; set => speed = value; }
+
+    private void Awake()
+    {
+        this.tr = this.gameObject.transform;
+    }
+
+     void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            AxleForward();     
+        }
+    }
+
+    private float GetSelfForwardAxis()
+    {
+        return this.transform.local;
+    }
+
+    private void AxleForward()
+    {
+       
+    }
+
+}
